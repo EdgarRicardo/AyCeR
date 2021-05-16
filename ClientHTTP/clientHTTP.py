93351b8ee9df7fd2ee991357d85db998c127e678
Edgar Corona <coronam.edgar@gmail.com>
2021-05-16 17:44:42 -0500

    Beautifulsoup

--- ClientHTTP/clientHTTP.py
+++ ClientHTTP/clientHTTP.py
@@ -3,23 +3,42 @@ import concurrent.futures
 from concurrent.futures import ThreadPoolExecutor
 import os
 import threading
-from requests.exceptions import HTTPError  
+from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup
+from pprint import pprint
 
 visitados = []
 pool = ThreadPoolExecutor(max_workers=30)
+etiquetasToFound = ['img','a']
 
-def analyseHTML(link,filename):
+def analyseHTML(link,html):
 	#Caso 1: /dddd/ddd -> url + linkFound -> listToCheck
 	#Caso 2: ddddd/dddd -> url + /linkFound -> listToCheck
 	#Caso 3: https://sdfdfdfssdfsdf.xxx/fcfasff -> listToCheck
 	# http://dfsdfsdfsdf.ccc/sdfsdfsdf/dfdfsdf -> listToCheck
 	#listToCheck = ["https://www.cdc.gov/coronavirus/2019-ncov/testing/index.html","http://www.google.com/","http://www.researchgate.net/profile/M_Gotic/publication/260197848_Mater_Sci_Eng_B47_%281997%29_33/links/0c9605301e48beda0f000000.pdf"]
+	# Edgar's code
 	urlSplit = link.split("/")[0:3]
 	url = "/".join(urlSplit)
 	listToCheck = []
-	html = open(filename,"r")
-	# Edgar's code
-	html.close()
+	#html = open(filename,"rb")
+	soup = BeautifulSoup(html)
+	for etiqueta in etiquetasToFound:
+		for link in soup.findAll(etiqueta):
+			urlFound = None
+			if link.get('href'):
+				urlFound = link.get('href')
+			elif link.get('src'):
+				urlFound = link.get('src')
+
+			if urlFound:
+				if 'http' in urlFound:
+					listToCheck.append(urlFound)
+				elif urlFound[0] == "/":
+					listToCheck.append(url+urlFound)
+				else:
+					listToCheck.append(url+"/"+urlFound)
+	#html.close()
 	return listToCheck
 
 def saveContentFile(content,directory,filename):
@@ -50,7 +69,7 @@ def getFiles(link):
 					filename += "/index.html"
 					path  = raiz + "/".join(splitLink[3:])
 				saveContentFile(res.content,path,filename)
-				listLinks = analyseHTML(link,filename)
+				listLinks = analyseHTML(link,res.content)
 			else:
 				print("File")
 				saveContentFile(res.content,path,filename)
@@ -78,10 +97,10 @@ def poolThreads(listLinks):
 
 if __name__ == "__main__":
 	#link = input("¿Dame tu link?: ")
-	link = "http://www.researchgate.net/profile/M_Gotic/publication/260197848_Mater_Sci_Eng_B47_%281997%29_33/links/0c9605301e48beda0f000000.pdf"
-	link = "https://es.wikipedia.org/wiki/Page"
+	#link = "http://www.google.com/"
+	#link = "http://www.researchgate.net/profile/M_Gotic/publication/260197848_Mater_Sci_Eng_B47_%281997%29_33/links/0c9605301e48beda0f000000.pdf"
+	#link = "https://es.wikipedia.org/wiki/Page"
 	link = "https://www.cdc.gov/coronavirus/2019-ncov/testing/index.html"
-	link = "http://www.google.com/"
 	listLinks = getFiles(link)
 	poolThreads(listLinks)
 
